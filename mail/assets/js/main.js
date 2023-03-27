babb6e8be4e7d7ea4a1179cdb7a78b77fe2d5318
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-27 17:48:08 +0200

    Esercizio mail - utilizzo del form

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -7,33 +7,71 @@
     - Con un if...else stampo il messaggio
 */
 
+// PARTE INIZIALE - CODICE REALIZZATO PER FUNZIONARE IN CONSOLE E CON L'UTILIZZO DEL PROMPT
 
-// comincio utilizzando la console per risolvere l'esercizio
+// // comincio utilizzando la console per risolvere l'esercizio
+// // costruisco un ipotetico database di indirizzi email
+// let email = ["pinco@yahoo.it", "pallo@gmail.com", "panco@yahoo.it", "tizio@gmail.com", "caio@gmail.com"]
+
+// // stampo in console il database
+// console.log(email)
+
+// // chiedo all'utente di inserire la propria mail
+// let myEmail = prompt("inserisci la tua mail")
+
+// // stampo in console la mail digitata dall'utente
+// console.log(myEmail)
+
+// // inizializzo la variabile "match" che mi servirà per il ciclo for successivo
+// let match = false;
+
+// // con il seguente ciclo for vado a scorrere il database di email creato (array) per confrontarlo con la mail digitata dall'utente. Il ciclo for mi restituisce o un valore true (esiste un matching) o un valore false (non esiste un matching)
+// for(let i = 0; i <= email.length - 1; i++){
+//   if (email[i] == myEmail) {
+//     match = true;
+//   }
+// }
+
+// // con la seguente istruzione condizionale vado a stampare in console il risultato.
+// if(match){
+//   console.log(`la mail corrisponde: puoi accedere!`)
+// } else{
+//   console.log(`la mail non corrisponde: registrati e poi accedi`)
+// }
+
+
+// UTILIZZO DEI TAG INPUT DELL'HTML
+
 // costruisco un ipotetico database di indirizzi email
 let email = ["pinco@yahoo.it", "pallo@gmail.com", "panco@yahoo.it", "tizio@gmail.com", "caio@gmail.com"]
 
 // stampo in console il database
 console.log(email)
 
-// chiedo all'utente di inserire la propria mail
-let myEmail = prompt("inserisci la tua mail")
+//creo la funzione che verrà associata al button tramite onclick
 
-// stampo in console la mail digitata dall'utente
-console.log(myEmail)
+function myFunction(){
+  // salvo in una variabile la mail digitata dall'utente nel tag input del form
+  let myEmail = document.getElementById("e-mail").value;
+  
+  // stampo in console la mail digitata dall'utente
+  console.log(myEmail);
 
-// inizializzo la variabile "match" che mi servirà per il ciclo for successivo
-let match = false;
+  // // inizializzo la variabile "match" che mi servirà per il ciclo for successivo
+  let match = false;
 
-// con il seguente ciclo for vado a scorrere il database di email creato (array) per confrontarlo con la mail digitata dall'utente. Il ciclo for mi restituisce o un valore true (esiste un matching) o un valore false (non esiste un matching)
-for(let i = 0; i <= email.length - 1; i++){
-  if (email[i] == myEmail) {
-    match = true;
+  // // con il seguente ciclo for vado a scorrere il database di email creato (array) per confrontarlo con la mail digitata dall'utente. Il ciclo for mi restituisce o un valore true (esiste un matching) o un valore false (non esiste un matching)
+  for(let i = 0; i <= email.length - 1; i++){
+    if (email[i] == myEmail) {
+      match = true;
+    }
+  }
+
+  // // con la seguente istruzione condizionale vado a comunicare il risultato.
+  if(match){
+    document.getElementById("results").innerHTML= `<h1 class="text-success">La mail corrisponde: puoi accedere!</h1>`
+  } else{
+    document.getElementById("results").innerHTML = `<h1 class="text-danger">La mail non corrisponde: registrati e poi accedi</h1>`
   }
 }
 
-// con la seguente istruzione condizionale vado a stampare in console il risultato.
-if(match){
-  console.log(`la mail corrisponde: puoi accedere!`)
-} else{
-  console.log(`la mail non corrisponde: registrati e poi accedi`)
-}
