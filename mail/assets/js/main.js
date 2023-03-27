c4d1f11ac9d02eeeee17220eaaf7a67e5e0e30d4
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-27 16:14:57 +0200

    Upload files

---
+++ mail/assets/js/main.js
@@ -0,0 +1,20 @@
+/*
+  1. Creo un array contente una serie di indirizzi Mail
+  2. Chiedi all’utente la sua email.
+    - utilizzo un form con input
+  3. Controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato sull’esito del        controllo.
+    - Utilizzando il tasto "submit" del form faccio il scorro l'array utilizzando un ciclo for.
+    - Con un if...else stampo il messaggio
+*/
+
+
+
+// comincio utilizzando la console per risolvere l'esercizio
+
+let email = ["pinco@yahoo.it", "pallo@gmail.com", "panco@yahoo.it", "tizio@gmail.com", "caio@gmail.com"]
+//
+console.log(email)
+//
+let myEmail = prompt("inserisci la tua mail")
+//
+console.log(myEmail)
