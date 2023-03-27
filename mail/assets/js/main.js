9316856b9f4b13cd4b74dfa51ce186651f79c43e
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-27 17:00:08 +0200

    Esercizio Mail - codice JS - console

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -8,13 +8,32 @@
 */
 
 
-
 // comincio utilizzando la console per risolvere l'esercizio
-
+// costruisco un ipotetico database di indirizzi email
 let email = ["pinco@yahoo.it", "pallo@gmail.com", "panco@yahoo.it", "tizio@gmail.com", "caio@gmail.com"]
-//
+
+// stampo in console il database
 console.log(email)
-//
+
+// chiedo all'utente di inserire la propria mail
 let myEmail = prompt("inserisci la tua mail")
-//
+
+// stampo in console la mail digitata dall'utente
 console.log(myEmail)
+
+// inizializzo la variabile "match" che mi servirà per il ciclo for successivo
+let match = false;
+
+// con il seguente ciclo for vado a scorrere il database di email creato (array) per confrontarlo con la mail digitata dall'utente. Il ciclo for mi restituisce o un valore true (esiste un matching) o un valore false (non esiste un matching)
+for(let i = 0; i <= email.length - 1; i++){
+  if (email[i] == myEmail) {
+    match = true;
+  }
+}
+
+// con la seguente istruzione condizionale vado a stampare in console il risultato.
+if(match){
+  console.log(`la mail corrisponde: puoi accedere!`)
+} else{
+  console.log(`la mail non corrisponde: registrati e poi accedi`)
+}
